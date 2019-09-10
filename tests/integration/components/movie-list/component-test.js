4581dba2acc63d4719da5fbbf7737705c4534632
Ismael <ismael.rm.1986@gmail.com>
2019-09-10 20:56:13 +0200

    Add test for movie-list  and movie-list-item

--- tests/integration/components/movie-list/component-test.js
+++ tests/integration/components/movie-list/component-test.js
@@ -1,15 +1,12 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, findAll } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | movie-list', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`{{movie-list}}`);
 
     assert.ok(this.$().length);
@@ -17,37 +14,11 @@ module('Integration | Component | movie-list', function(hooks) {
 
 
   test('Show list of movies', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-    const moviesSorted = [
-      {
-        title: 'kill bill',
-        description: 'Description for kill bill 1',
-        image: 'http://images.org/kill-bill',
-        rating: '5',
-      },
-      {
-        title: 'kill bill 2',
-        description: 'Description for kill bill 2',
-        image: 'http://images.org/kill-bill',
-        rating: '4',
-      },
-      {
-        title: 'Once upon a time .. Hollywood ',
-        description: 'Description Once upon a time ... Holywood',
-        image: 'http://images.org/once-upon-a-time',
-        rating: '3',
-      },
-      {
-        title: 'Pull Fiction',
-        description: 'Description for Pull fiction',
-        image: 'http://images.org/pull-fiction',
-        rating: '2',
-      }
-    ];
-
-    await render(hbs`{{movie-list}}`);
-    this.set('movies',moviesSorted);
-    assert.ok(this.$().length);
+    const store = this.owner.lookup('service:store');
+    const movies = await store.findAll('movie');
+    
+    this.set('movies',movies)
+    await render(hbs`{{movie-list movies=movies}}`);
+    assert.ok(findAll('li').length > 0);
   });
 });
--- tests/integration/components/movie-list/movie-list-item/component-test.js
+++ tests/integration/components/movie-list/movie-list-item/component-test.js
@@ -1,32 +1,25 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, findAll } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | movie-list/movie-list-item', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`{{movie-list/movie-list-item}}`);
-
     assert.ok(this.$().length);
   });
 
-   /*test('Show movie', async function(assert) {
-    const movieData = [
-      {
-        title: 'kill bill',
-        description: 'Description for kill bill 1',
-        image: 'http://images.org/kill-bill',
-        rating: '5',
-      }
-    ];
+   test('Show movie', async function(assert) {
+    const store = this.owner.lookup('service:store');
+    const movies = await store.findAll('movie');
+    const id = movies.objectAt(0).id;
+    const movie = await store.find('movie', id);
 
-    await render(hbs`{{movie-list-item movie=movieData}}`);
-    const component = this.owner.lookup('component:movie-list/movie-list-item');
-    assert.ok(this.$().length);
-  });*/
+    this.set('movie', movie);
+    await render(hbs`{{movie-list/movie-list-item movie=movie}}`);
+    assert.ok(findAll('li').length === 1);
+  });
+
 });
