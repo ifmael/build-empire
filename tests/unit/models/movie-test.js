5edbe6499da479362ced3948ac674984544d0a40
Ismael <ismael.rm.1986@gmail.com>
2019-09-10 20:55:41 +0200

    Add test for model

--- tests/unit/models/movie-test.js
+++ tests/unit/models/movie-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import { run } from '@ember/runloop';
 
+
 module('Unit | Model | movie', function(hooks) {
   setupTest(hooks);
 
@@ -11,4 +12,39 @@ module('Unit | Model | movie', function(hooks) {
     let model = run(() => store.createRecord('movie', {}));
     assert.ok(model);
   });
+
+  test('There is any movie', async function(assert){
+    const store = this.owner.lookup('service:store');
+    const movies = await store.findAll('movie');
+    assert.ok(movies.length > 0);
+  });
+
+  test('Create a new fake movie', async function(assert){
+    const store = this.owner.lookup('service:store');
+    const record = await store.createRecord('movie', {
+      title:' fake movie', description: 'fake description', image: 'fake image', rating: 'fake rating'
+    });
+    assert.ok(record.isNew);
+    record.rollbackAttributes();
+    assert.notOk(record.isNew);
+  });
+
+  test('Find a specific movie', async function(assert){
+    const store = this.owner.lookup('service:store');
+    const movies = await store.findAll('movie');
+    const id = movies.objectAt(0).id;
+    const movie = await store.find('movie', id);
+    assert.notOk(movie.isEmpty);
+    assert.ok(movie.isValid);
+  });
+
+  test('Delete a specific movie', async function(assert){
+    const store = this.owner.lookup('service:store');
+    const movies = await store.findAll('movie');
+    const movie = movies.objectAt(0);
+    assert.notOk(movie.isDeleted);
+    movie.deleteRecord();
+    assert.ok(movie.isDeleted);
+  });
+
 });
