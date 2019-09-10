3a7b8d281ffbe1b79d4c1e503ff39c0c1d44cf6e
Ismael <ismael.rm.1986@gmail.com>
2019-09-10 20:56:55 +0200

    Fix test route

--- tests/unit/movie/route-test.js
+++ tests/unit/movie/route-test.js
@@ -5,7 +5,7 @@ module('Unit | Route | movie', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('movie:movie');
+    let route = this.owner.lookup('route:movie');
     assert.ok(route);
   });
 });
